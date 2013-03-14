04410ba587a18476d9508153206efa3160bef004
Matthew Roscoe <badrobot@shadow1.(none)>
2013-03-14 14:00:06 +0100

    Started to modify the visual servoing in order to add a library for 3D visual servoing.

---
+++ common/include/VisualServoing3D.h
@@ -0,0 +1,18 @@
+/*
+ * VisualServoing3D.h
+ *
+ *  Created on: Mar 14, 2013
+ *      Author: badrobot
+ */
+
+#ifndef VISUALSERVOING3D_H_
+#define VISUALSERVOING3D_H_
+
+class VisualServoing3D
+{
+public:
+	VisualServoing3D();
+	virtual ~VisualServoing3D();
+};
+
+#endif /* VISUALSERVOING3D_H_ */
---
+++ common/src/VisualServoing3D.cpp
@@ -0,0 +1,20 @@
+/*
+ * VisualServoing3D.cpp
+ *
+ *  Created on: Mar 14, 2013
+ *      Author: badrobot
+ */
+
+#include "VisualServoing3D.h"
+
+VisualServoing3D::VisualServoing3D()
+{
+	// TODO Auto-generated constructor stub
+
+}
+
+VisualServoing3D::~VisualServoing3D()
+{
+	// TODO Auto-generated destructor stub
+}
+
